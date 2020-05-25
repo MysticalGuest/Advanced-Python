f1a261e440349b9f69dc547f9b763e025573682e
MysticalGuest <1317148109@qq.com>
2020-05-25 15:02:21 +0800

    Create Set_Function.py

---
+++ CourseReview/Set_Function.py
@@ -0,0 +1,10 @@
+empty_set = set()
+print(empty_set)
+even_numbers = {0, 2, 4, 6, 8}
+print(even_numbers)
+odd_numbers = {1, 3, 5, 7, 9}
+print(odd_numbers)
+
+print(set('letters'))
+print(set(['l', 'e', 't', 't', 'e', 'r', 's']))
+
