6800a13eb2966b5f825ea0b1c6c5b51d9cae696c
MysticalGuest <1317148109@qq.com>
2020-05-25 15:02:37 +0800

    Create tk_canvas.py

---
+++ Tkinter/tk_canvas.py
@@ -0,0 +1,16 @@
+# tk_canvas.py
+from tkinter import *
+master = Tk()
+
+canvas_width = 80
+canvas_height = 40
+w = Canvas(master, 
+           width=canvas_width,
+           height=canvas_height)
+w.pack()
+
+y = int(canvas_height / 2)
+w.create_line(0, y, canvas_width, y, fill="#476042")
+
+
+mainloop()
