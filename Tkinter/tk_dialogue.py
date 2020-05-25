0c2863366f7f1c9a8114246b1f34ed0898922eb3
MysticalGuest <1317148109@qq.com>
2020-05-25 15:03:03 +0800

    Create tk_dialogue.py

---
+++ Tkinter/tk_dialogue.py
@@ -0,0 +1,16 @@
+# tk_dialogue.py
+from tkinter import *
+from tkinter.messagebox import *
+
+def answer():
+    showerror("Answer", "Sorry, no answer available")
+
+def callback():
+    if askyesno('Verify', 'Really quit?'):
+        showwarning('Yes', 'Not yet implemented')
+    else:
+        showinfo('No', 'Quit has been cancelled')
+
+Button(text='Quit', command=callback).pack(fill=X)
+Button(text='Answer', command=answer).pack(fill=X)
+mainloop()
