eb048af709fab384112503cbc1a4110eb7eae3c3
MysticalGuest <1317148109@qq.com>
2020-05-25 15:04:10 +0800

    Create tk_radio_button3.py

---
+++ Tkinter/tk_radio_button3.py
@@ -0,0 +1,36 @@
+# tk_radio_button3.py - only differs from tk_radio_button2 in
+# tk.Radiobutton
+import tkinter as tk
+root = tk.Tk()
+v = tk.IntVar()
+v.set(1)  # initializing the choice, i.e. Python
+
+languages = [
+    ("Python",1),
+    ("Perl",2),
+    ("Java",3),
+    ("C++",4),
+    ("C",5)
+]
+
+def ShowChoice():
+    print(v.get())
+
+tk.Label(root, 
+         text="""Choose your favourite 
+programming language:""",
+         justify = tk.LEFT,
+         padx = 20).pack()
+
+for val, language in enumerate(languages):
+    tk.Radiobutton(root,  # JUST THIS PART IS DIFFERENT
+                  text=language,
+                  indicatoron = 0,
+                  width = 20,
+                  padx = 20, 
+                  variable=v, 
+                  command=ShowChoice,
+                  value=val).pack(anchor=tk.W)
+
+
+root.mainloop()
