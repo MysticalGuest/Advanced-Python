5d1130758efda8dfaf4c93aab7809c5c8c323954
MysticalGuest <1317148109@qq.com>
2020-07-02 22:47:14 +0800

    Update lesson12.py

--- BigDataAnalysis/Pandas/lesson12.py
+++ BigDataAnalysis/Pandas/lesson12.py
@@ -1,67 +1,77 @@
+import numpy as np
 import pandas as pd
+# 异常值处理方法1--拉依达准则函数
+import warnings
 
 
-# 3.数据清洗
-# 3-1  重复值的检测和处理
+warnings.filterwarnings('ignore')
+
+
+def out_range(data):
+    arr_bool = (data.mean() - 3 * data.std() > data) | (data.mean() + 3 * data.std() < data)  # 确定了异常值的布尔值
+
+    index = np.arange(data.shape[0])[arr_bool]
+    out_v1 = data.iloc[index]
+    return out_v1
+
+
 detail1 = pd.read_csv("meal_order_detail1.csv", sep=",", encoding="utf-8")
-print("detail1: \n", detail1)
+detail1_v2 = detail1.dropna(how="all", axis=1)
 
-print("detail1['dishes_name'].describe(): \n", detail1['dishes_name'].describe())
+data1 = detail1_v2['amounts']
+out_v2 = out_range(data1)
+print(out_v2)
+print(out_v2.shape)
+print(out_v2.max())
+print(out_v2.min())
 
-# 去重方法1：分组方法
-list(detail1.groupby('dishes_name', sort=False)['dishes_name'].describe().index)
+data2 = detail1_v2['counts']     # 销售个数
 
-# 去重方法2：列表方法
+out_v3 = out_range(data2)
+print(out_v2)
+print(out_v3.shape)    # 共62个异常值
+print(out_v3.max())    # 最大异常值10
+print(out_v3.min())    # 最小的异常值是3
 
+# 处理方法：将大于范围的异常值，修改取值范围的上限值
+data2[data2 > (data2.mean()+3*data2.std())] = (data2.mean()+3*data2.std())
+data3 = data2
+(data3 > 5).sum()
 
-def del_rep(list1):
-    list2 = []   # 存放去重后的结果
-    for i in list1:
-        if i not in list2:
-            list2.append(i)
-    return list2
+# 处理方法：将大于范围的异常值，修改取值范围的下限值
+data2[data2 > (data2.mean()+3*data2.std())] = (data2.mean()+3*data2.std())
+data3 = data2
+(data3 > 5).sum()
 
-
-name_list = list(detail1['dishes_name'])
-print("len(name_list): ", len(name_list))
-resu_list = del_rep(name_list)
-print("len(resu_list): ", len(resu_list))
-print("列表去重后的结果\n", resu_list)
-
-# 去重方法3：集合方法
-resu_list2 = set(name_list)
-print("集合去重后的结果\n", resu_list2)
-
-# 去重方法4：pandas方法
-print(type(detail1['dishes_name'].drop_duplicates()))     # 去重之后得到的是一个Series，把对应的索引也取出了。
-index1 = detail1['dishes_name'].drop_duplicates(keep='first').index    # 得到取出结果的索引index
-print(detail1.loc[index1, :].shape)     # 用索引取df的所有值
-
-# 去食堂吃饭，10个人都要了烤肉   ：只出现烤肉一条信息
-# 取每个订单中的烤肉
-# 有10条烤肉信息
-
-# 3-2
+# 上面函数的分布操作
+data2 = detail1_v2['counts']
+min_v1 = data2.mean()-3*data2.std()       # 0.0719245104270092    范围最小值
+max_v2 = data2.mean()+3*data2.std()      # 2.073612301017749    范围最大值
 
 
-def put_data_def(file_name):
-    pd.set_option('display.unicode.ambiguous_as_wide', True)
-    pd.set_option('display.unicode.east_asian_width', True)
-    print("1.csv文件 \n 2.excel文件\n")
-    type_v1 = input("请输入您要打开的文件类型：")
-    if type_v1 == "1":
-        df_csv = pd.read_csv("{}.csv".format(file_name, sep=","))
-        return df_csv
-    elif type_v1 == "2":
-        df_excel = pd.read_excel("{}.xlsx".format(file_name))
-        return df_excel
+(data2 < min_v1).sum()       # 统计小于最小值的个数
+(data2 > max_v2).sum()       # 统计大于最大值的个数
+data2[data2 > max_v2] = max_v2      # 将异常值进行处理
+(data2 > 3).sum()        # 验证处理结果
 
 
-df_excel = put_data_def("超市营业额")
-print("原始数据：\n", df_excel)
-print("原来数据的形状：\n", df_excel.shape)
-print("删除重复值后的形状：\n", df_excel.drop_duplicates().shape)
-# 找出重复值是哪一行
-print("重复的一行", df_excel[df_excel.duplicated() == True])
+# 处理异常值的第2中方法
+def out_range1(data):
+    QL = data.quantile(0.25)  # 下四分位
+    QU = data.quantile(0.75)  # 上四分位
+    IQR = QU - QL  # 四分位间距
 
+    data.loc[data > (QU + 1.5 * IQR)] = QU
+    data.loc[data < (QL - 1.5 * IQR)] = QL
+    return data
+
+
+# data4 = detail1_v2['amounts']
+data4 = out_range1(data1)
+print("data4: \n", data4)
+
+detail1_v2['amounts'].describe()
+
+print("data4.quantile(0.25): \n", data4.quantile(0.25))
+print("data4.quantile(0.75): \n", data4.quantile(0.75))
 
