e05c073eafabb34274ab30483a4e6d7d487e2e8d
MysticalGuest <1317148109@qq.com>
2020-07-02 22:46:58 +0800

    Update lesson1.py

--- BigDataAnalysis/Pandas/lesson1.py
+++ BigDataAnalysis/Pandas/lesson1.py
@@ -67,3 +67,27 @@ for i in range(len(seri_v2)):
                  xytext=(i, seri_v2[i]+2))   # 箭头的起点坐标
 plt.show()
 
+
+# 设置显示文本对齐
+pd.set_option('display.unicode.ambiguous_as_wide', True)
+pd.set_option('display.unicode.east_asian_width', True)
+
+# 4.写数据到本地磁盘文件
+# 2-1  方式1：用数组创建
+df_v1 = pd.DataFrame(np.random.randint(10, 100, size=(10, 3)),
+                     index=np.arange(0, 10, 1),
+                     columns=['语文', '数学', "英语"])
+print("df_v1的原始数据".ljust(30, "="), "\n")
+print(df_v1)
+
+# 2-2  方式2：用字典创建
+df_dict = {"熟食": list(np.random.randint(10, 100, size=15)),
+           "化妆品": list(np.random.randint(100, 200, size=15)),
+           "日用品": list(np.random.randint(10, 100, size=15))}
+
+df_v2 = pd.DataFrame(df_dict, index=pd.date_range(start="20200615",
+                                                  end="20200629",
+                                                  freq="D"))
+print("df_v2的原始数据".ljust(30, "="), "\n")
+print(df_v2)
+
