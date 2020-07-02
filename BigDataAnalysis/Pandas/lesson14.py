d74ed4b8c2a029a759ae6fafd5a9f2d806fb1c55
MysticalGuest <1317148109@qq.com>
2020-07-02 22:47:21 +0800

    Update lesson14.py

--- BigDataAnalysis/Pandas/lesson14.py
+++ BigDataAnalysis/Pandas/lesson14.py
@@ -1,77 +1,77 @@
-import numpy as np
+# 分析一个公司中，员工的年龄和工资的关系
 import pandas as pd
-# 异常值处理方法1--拉依达准则函数
-import warnings
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(45)
+dict_data = {"年龄": list(np.random.randint(18, 60, 40)),
+             "工资": list(np.random.randint(4000, 30000, 40))}
+
+df_data = pd.DataFrame(dict_data)
+print("df_data: \n", df_data)
+
+# 给以上数据绘图
+fig = plt.figure(figsize=(10, 10), dpi=100)
+
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
+ax1.plot(df_data.index, df_data["年龄"])
+ax2.plot(df_data.index, df_data["工资"], "r-.")
+plt.plot(df_data.index, df_data["年龄"])
+plt.plot(df_data.index, df_data["工资"], "r-.")
+plt.show()
 
 
-warnings.filterwarnings('ignore')
-
-
-def out_range(data):
-    arr_bool = (data.mean() - 3 * data.std() > data) | (data.mean() + 3 * data.std() < data)  # 确定了异常值的布尔值
-
-    index = np.arange(data.shape[0])[arr_bool]
-    out_v1 = data.iloc[index]
-    return out_v1
-
-
-detail1 = pd.read_csv("meal_order_detail1.csv", sep=",", encoding="utf-8")
-detail1_v2 = detail1.dropna(how="all", axis=1)
-
-data1 = detail1_v2['amounts']
-out_v2 = out_range(data1)
-print(out_v2)
-print(out_v2.shape)
-print(out_v2.max())
-print(out_v2.min())
-
-data2 = detail1_v2['counts']     # 销售个数
-
-out_v3 = out_range(data2)
-print(out_v2)
-print(out_v3.shape)    # 共62个异常值
-print(out_v3.max())    # 最大异常值10
-print(out_v3.min())    # 最小的异常值是3
-
-# 处理方法：将大于范围的异常值，修改取值范围的上限值
-data2[data2 > (data2.mean()+3*data2.std())] = (data2.mean()+3*data2.std())
-data3 = data2
-(data3 > 5).sum()
-
-# 处理方法：将大于范围的异常值，修改取值范围的下限值
-data2[data2 > (data2.mean()+3*data2.std())] = (data2.mean()+3*data2.std())
-data3 = data2
-(data3 > 5).sum()
-
-# 上面函数的分布操作
-data2 = detail1_v2['counts']
-min_v1 = data2.mean()-3*data2.std()       # 0.0719245104270092    范围最小值
-max_v2 = data2.mean()+3*data2.std()      # 2.073612301017749    范围最大值
-
-
-(data2 < min_v1).sum()       # 统计小于最小值的个数
-(data2 > max_v2).sum()       # 统计大于最大值的个数
-data2[data2 > max_v2] = max_v2      # 将异常值进行处理
-(data2 > 3).sum()        # 验证处理结果
-
-
-# 处理异常值的第2中方法
-def out_range1(data):
-    QL = data.quantile(0.25)  # 下四分位
-    QU = data.quantile(0.75)  # 上四分位
-    IQR = QU - QL  # 四分位间距
-
-    data.loc[data > (QU + 1.5 * IQR)] = QU
-    data.loc[data < (QL - 1.5 * IQR)] = QL
+# 写离差标准化函数
+def min_max_scale(data):
+    data = (data-data.min())/(data.max()-data.min())
     return data
 
 
-# data4 = detail1_v2['amounts']
-data4 = out_range1(data1)
-print("data4: \n", data4)
+# 标准差标准化
+def stand_scale(data):
+    data = (data-data.mean())/data.std()
+    return data
 
-detail1_v2['amounts'].describe()
 
-print("data4.quantile(0.25): \n", data4.quantile(0.25))
-print("data4.quantile(0.75): \n", data4.quantile(0.75))
+# 小数定准标准化
+def decimal_scale(data):
+    data = data/10**np.ceil(np.log10(data.abs().max()))
+    return data
+
+
+df_data = pd.DataFrame(dict_data)
+
+df_data['年龄'] = decimal_scale(df_data['年龄'])
+df_data['工资'] = decimal_scale(df_data['工资'])
+
+fig2 = plt.figure(figsize=(10, 10), dpi=100)
+
+ax1 = fig2.add_subplot(221)
+ax2 = fig2.add_subplot(222)
+ax3 = fig2.add_subplot(212)
+ax1.plot(df_data.index, df_data["年龄"])
+ax2.plot(df_data.index, df_data["工资"], "r-.")
+ax3.plot(df_data.index, df_data["年龄"], "b--",
+         df_data["工资"], "r-.", alpha=0.8)
+plt.show()
+
+# 离散化连续型数据
+data5 = df_data['年龄']
+# 方法1：等宽法
+df_data['年龄'] = pd.cut(data5, 4, labels=['青年', '中年', '老年', '领导'])
+df_data.groupby("年龄").describe()
+
+
+# 方法2：等频法划分
+def same_cut(data, k):
+    w = data.quantile(np.arange(0, 1+1.0/k, 1.0/k))
+    data = pd.cut(data, w)
+    return data
+
+
+results = same_cut(data5, 5).value_counts()
+print(results)
+
+print("10**0.2: \n", 10**0.2)
 
