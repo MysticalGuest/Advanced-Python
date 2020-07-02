c92406989711de28714a5c38131b50989eefa813
MysticalGuest <1317148109@qq.com>
2020-07-02 22:47:07 +0800

    Update lesson10.py

--- BigDataAnalysis/Pandas/lesson10.py
+++ BigDataAnalysis/Pandas/lesson10.py
@@ -1,48 +1,67 @@
 import pandas as pd
 
 
-# 是否使用Unicode东亚宽度来计算显示文本的宽度。 启用它可能会影响性能（默认：False）[默认：False] [当前：True]
-pd.set_option('display.unicode.ambiguous_as_wide', True)
-# pd.set_option('display.unicode.east_asian_width', True)
+# 3.数据清洗
+# 3-1  重复值的检测和处理
+detail1 = pd.read_csv("meal_order_detail1.csv", sep=",", encoding="utf-8")
+print("detail1: \n", detail1)
 
-df1 = pd.read_excel('超市营业额.xlsx')
-print("打开的第1个数据：\n", df1)
+print("detail1['dishes_name'].describe(): \n", detail1['dishes_name'].describe())
 
-# sheet_name对应Excel中的Sheet标签，一个excel可能有不止一个sheet
-# sheet_name参数可以接收的有：str，int，list或None，默认0
-# 指定读取第一个位置的sheet
-df2 = pd.read_excel("超市营业额.xlsx", sheet_name=1)
-print("打开的第2个数据：\n", df2)
+# 去重方法1：分组方法
+list(detail1.groupby('dishes_name', sort=False)['dishes_name'].describe().index)
 
-# 1.合并数据命令1-concate()
-df3 = pd.concat([df1, df2], axis=0, ignore_index=True)
-print("使用concate合并的数据，将df1和df2合并:\n", df3)
-
-# 2.合并数据命令2-append()
-df6 = df1.loc[df1['日期'] == "2019-03-01", :]     # 提取出2019年3月1日的数据，和2019年4月1日的数据合并
-print("df6：\n", df6)
-df4 = df6.append(df2, ignore_index=True)   # ignore_index的作用是忽略原来的索引。
-print("使用append合并的数据：\n", df4)
-
-# 3.合并数据命令3-merge()
-df5 = pd.read_excel("超市营业额.xlsx", sheet_name=2)
-print("df5：\n", df5)
-
-df7 = pd.merge(df3, df5, on="工号", how="left")
-print("df7：\n", df7)
-
-# 4.合并数据命令4-join
-df8 = df3.set_index("工号").join(df5.set_index("工号"), lsuffix="_x", rsuffix="_y")
-print("df8：\n", df8)
-
-# 5.重叠合并--combine_first()
+# 去重方法2：列表方法
 
 
-# 补充知识点：设置索引
-df7 = pd.merge(df3, df5, how="left")
-df9 = df7.set_index('柜台')
-print("df9：\n", df9)
-# 重置索引
-df9.reset_index()
-print("df9：\n", df9)
+def del_rep(list1):
+    list2 = []   # 存放去重后的结果
+    for i in list1:
+        if i not in list2:
+            list2.append(i)
+    return list2
+
+
+name_list = list(detail1['dishes_name'])
+print("len(name_list): ", len(name_list))
+resu_list = del_rep(name_list)
+print("len(resu_list): ", len(resu_list))
+print("列表去重后的结果\n", resu_list)
+
+# 去重方法3：集合方法
+resu_list2 = set(name_list)
+print("集合去重后的结果\n", resu_list2)
+
+# 去重方法4：pandas方法
+print(type(detail1['dishes_name'].drop_duplicates()))     # 去重之后得到的是一个Series，把对应的索引也取出了。
+index1 = detail1['dishes_name'].drop_duplicates(keep='first').index    # 得到取出结果的索引index
+print(detail1.loc[index1, :].shape)     # 用索引取df的所有值
+
+# 去食堂吃饭，10个人都要了烤肉   ：只出现烤肉一条信息
+# 取每个订单中的烤肉
+# 有10条烤肉信息
+
+# 3-2
+
+
+def put_data_def(file_name):
+    pd.set_option('display.unicode.ambiguous_as_wide', True)
+    pd.set_option('display.unicode.east_asian_width', True)
+    print("1.csv文件 \n 2.excel文件\n")
+    type_v1 = input("请输入您要打开的文件类型：")
+    if type_v1 == "1":
+        df_csv = pd.read_csv("{}.csv".format(file_name, sep=","))
+        return df_csv
+    elif type_v1 == "2":
+        df_excel = pd.read_excel("{}.xlsx".format(file_name))
+        return df_excel
+
+
+df_excel = put_data_def("超市营业额")
+print("原始数据：\n", df_excel)
+print("原来数据的形状：\n", df_excel.shape)
+print("删除重复值后的形状：\n", df_excel.drop_duplicates().shape)
+# 找出重复值是哪一行
+print("重复的一行", df_excel[df_excel.duplicated() == True])
+
 
