a1a04a77ffed9fed61676f5a62173f5720ebb70a
MysticalGuest <1317148109@qq.com>
2020-07-03 10:36:21 +0800

    Update case4.py

--- BigDataAnalysis/Pandas/case4.py
+++ BigDataAnalysis/Pandas/case4.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 
-dian_df = pd.read_csv("data_etr.csv",parse_dates=['DATA_DATE'])
+dian_df = pd.read_csv("data_etr.csv", parse_dates=['DATA_DATE'])
 
 # 用户用电量统计分析
 # data.csv为用户用电量数据,数据中有编号为1-200的200位电力用户，DATA_DATE表示时间，如2015/1/1表示2015年1月1日，KWH为用电量。
 # 1.	将数据进行转置，转置后行为用户编号、列为日期、值为用户每日用电量。
-dian_df1 = pd.pivot_table(dian_df,index="CONS_NO",columns="DATA_DATE",values="KWH")
+dian_df1 = pd.pivot_table(dian_df, index="CONS_NO", columns="DATA_DATE", values="KWH")
 # 2.	对数据中的异常数据进行识别并处理。
 
 # 3.	统计每个用户用电数据的基本统计量，包括：最大值、最小值、均值、中位数、和、方差、偏度、峰度。
@@ -18,5 +18,5 @@ dian_df1 = pd.pivot_table(dian_df,index="CONS_NO",columns="DATA_DATE",values="KW
 # 9.	求取每个用户七八月电量和与三四月电量和的比值，最大值的比值，最小值的比值，均值（日均电量）的比值。
 # 10.	合并上述特征。
 
-dian_df1
+print("dian_df1: \n", dian_df1)
 
