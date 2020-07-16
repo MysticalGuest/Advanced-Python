0bb6e78269757e39ae333db7105a883af95c16ab
MysticalGuest <1317148109@qq.com>
2020-07-16 17:09:29 +0800

    Update analysis3.py

--- BigDataAnalysis/Epidemic/analysis3.py
+++ BigDataAnalysis/Epidemic/analysis3.py
@@ -35,31 +35,37 @@ def element_def(data, ele_v1):
 
 
 # 函数3：转换为dataframe形式的数据
-def dataFrame_def(AllData, info_data):
-    alldata = pd.DataFrame(AllData)[info_data]
-
-    today_data = pd.DataFrame([item['today'] for item in AllData])  # 当天的数据
+def dataFrame_def(all_data, info_data):
+    # all_data = pd.DataFrame(all_data)[info_data]
+    print("all_data: \n", all_data)
+    for item in all_data:
+        print("item: ", item)
+        print("item['today']: ", item['today'])
+    today_data = pd.DataFrame([item['today'] for item in all_data])  # 当天的数据
     today_data.columns = ['当日_' + c for c in today_data.columns]
 
-    total_data = pd.DataFrame([item['total'] for item in AllData])  # 总的数据
+    total_data = pd.DataFrame([item['total'] for item in all_data])  # 总的数据
     total_data.columns = ['累计_' + c for c in total_data.columns]
 
     if info_data[0] != "name":
-        date_data = pd.DataFrame(AllData)['date']
+        date_data = pd.DataFrame(all_data)['date']
     else:
-        # id_data = pd.DataFrame(AllData)["id"]
-        date_data = pd.DataFrame(AllData)["name"]
+        # id_data = pd.DataFrame(all_data)["id"]
+        date_data = pd.DataFrame(all_data)["name"]
     #  date_data = pd.concat([id_data,name_data],axis=1)
-    #  date_data = pd.DataFrame(AllData)['date']
-    NewData = pd.concat([date_data, today_data, total_data], axis=1)  # 将today，total，info进行合并
-    return NewData
+    #  date_data = pd.DataFrame(all_data)['date']
+    new_data = pd.concat([date_data, today_data, total_data], axis=1)  # 将today，total，info进行合并
+    return new_data
 
 
 # 采集历史数据
 def hist_def():
     print("请输入您的选择".center(40, "="))
     print("您要采集什么历史数据：\n1.全国历史数据  \n2.中国各省 \n3.全球各国 \n0.退出  \n")
+    use_url = ''
+    ele_v1 = ''
     sel_v2 = input("请输入您的选择：")
+    sel_v3 = ''
     if sel_v2 == "1":  # 中国历史数据
         use_url = "https://c.m.163.com/ug/api/wuhan/app/data/list-total?t=318920407501"
         ele_v1 = 'chinaDayList'
@@ -77,7 +83,6 @@ def hist_def():
         use_url = "https://c.m.163.com/ug/api/wuhan/app/data/list-by-area-code?areaCode={}&t=1594602038577".format(66)
         ele_v1 = 'list'
         sel_v3 = "某国"
-
     page_data = page_def(use_url)  # 请求网页
     all_data = element_def(page_data, ele_v1)  # 解析元素
     df_data = dataFrame_def(all_data, ['date', 'today', 'total'])  # 转换为df
@@ -90,6 +95,7 @@ def real_def():
     use_url = "https://c.m.163.com/ug/api/wuhan/app/data/list-total?t=318920407501"
     print("请输入您的选择".center(40, "="))
     print("您要采集什么实时数据：\n1.全球的实时数据\n2.各国州/省\n")
+    ele_v1 = ''
     sel_v2 = input('请输入您的选择：')
     if sel_v2 == "1":  # 全球各国的实时数据
         ele_v1 = 'areaTree'
@@ -117,6 +123,7 @@ def main():
     print("请输入您的选择".center(40, "="))
     print("您要采集的数据是：\n1.历史数据  \n2.实时数据 \n0.退出  \n")
     sel_v1 = input("请输入您的选择：")
+    page_data = ''
     if sel_v1 == "1":
         page_data = hist_def()
 
@@ -128,10 +135,9 @@ def main():
         sys.exit("我先走了")
     else:
         print("没有您的选择")
-
     return page_data
 
 
 if __name__ == "__main__":
-    page_data = main()
-page_data
+    pageData = main()
+    print("pageData: \n", pageData)
